cb030b098707eb830283234c061d24e114bd9cd3
prof-anapaula <ana.stadler@escola.pr.gov.br>
2023-09-21 09:54:36 -0300

    Adicionei arquivos do Expo

---
+++ src/telas/Cesta.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Image} from 'react-native';
+
+import topo from '../../assets/topo.png';
+
+export default function Cesta(){
+    return <Image source={topo}/>
+}
